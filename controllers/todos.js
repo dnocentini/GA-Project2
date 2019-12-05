f7a90543392a5ac92b9ea080d79ba7f14baae590
Dinny Nocentini <dinny.nocentini@gmail.com>
2019-12-04 23:26:59 -0600

    More css updates

--- controllers/todos.js
+++ controllers/todos.js
@@ -4,26 +4,39 @@ module.exports = {
   index,
   create,
   delete: deleteTodo,
-  edit,
   update
 };
 
-function update(req, res) {
-  User.update(req.params.id, req.body);
-  res.redirect('/todos');
-};
-
-function edit(req, res) {
-  const todo = User.getOne(req.params.id);
-  res.render('todos/edit', {
-    todo,
-    idx: req.params.id
+function update(req, res, next) {
+  User.findById(req.session.passport.user, function (err, user) {
+    for (var i = 0; i < user.todos.length; i++) {
+      if (user.todos[i]._id == req.body._id) {
+        user.todos[i].content = req.body.content
+        break; 
+      }
+    }
+    user.save(function (err) {
+      if (err) return next(err)
+      res.redirect(`/todos`)
+    });
   });
 };
 
+// function update(req, res) {
+//   User.update(req.params.id, req.body);
+//   res.redirect('/todos');
+// };
+
+// function edit(req, res) {
+//   const todo = User.getOne(req.params.id);
+//   res.render('todos/edit', {
+//     todo,
+//     idx: req.params.id
+//   });
+// };
+
 function deleteTodo(req, res) {
   User.findById(req.session.passport.user).exec(function (err, user) {
-    // User.deleteOne({_id: req.params.id}, function (err, todos) {
     user.todos.id(req.params.id).remove();
     user.save(function (err) {
       if (err) return next(err);
@@ -36,15 +49,14 @@ function create(req, res, next) {
   for (let key in req.body) {
     if (req.body[key] === '') delete req.body[key];
   }
-  // var todo = new Todo(req.body);
   User.findById(req.session.passport.user).exec(function (err, user) {
     user.todos.push(req.body)
     user.save(function (err) {
       if (err) return next(err);
       res.render('todos/index', { user });
     });
-  })
-}
+  });
+};
 
 function index(req, res) {
   User.findById(req.session.passport.user).exec(function (err, user) {
--- routes/todos.js
+++ routes/todos.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const todosCtrl = require('../controllers/todos');
 
 router.get('/', todosCtrl.index);
-router.get('/:id/edit', todosCtrl.edit);
+// router.get('/:id/edit', todosCtrl.edit);
 router.post('/', todosCtrl.create);
 router.delete('/:id', todosCtrl.delete);
 router.put('/:id', todosCtrl.update);
